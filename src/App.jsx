99734f4d69a2dc7031683bb751781d6e0d2fdb75
SiiyaN <87692603+SiiyaN@users.noreply.github.com>
2024-12-29 21:41:30 +0200

    Added a footer

--- src/App.jsx
+++ src/App.jsx
@@ -35,6 +35,7 @@ export default function App() {
               {icon && <img src={icon} alt={description} />}
             </li>
           </ul>
+          <Footer />
         </div>
       )}
     </div>
